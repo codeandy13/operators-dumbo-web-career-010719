82c2f67a27ea08eed190d16d45f60784986a91c4
Andrew Kim <akim618@gmail.com>
2018-12-17 20:11:06 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -12,9 +12,10 @@ end
 end
 
 def not_safe?(speed)
-	speed >= 41 && speed <= 59? true : false
+	if speed >= 41 && speed <=59
+		return false
 
-	if speed > 60
+	elsif speed > 60
 		return true
 
 	elsif speed < 40
